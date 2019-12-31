bd7f23dc5a57b458e13f7e6dfb3441efa5e0d604
jamesdobson <dobson@softwarepunk.com>
2019-12-31 16:55:50 -0500

    ✏️  Correcting SHA-256 checksum.

--- Casks/panoptes.rb
+++ Casks/panoptes.rb
@@ -1,6 +1,6 @@
 cask 'panoptes' do
     version '1.0.0'
-    sha256 '8569ce3eac92961e9b0c8c2dbab13a3a2dd4523dc3ae2d575b37d7a81c0ceba4'
+    sha256 'bf8506e10a35ccca828297c31943ca62829468afd98776ed9153083c3d2e1e84'
 
     url "https://github.com/jamesdobson/panoptes/releases/download/v#{version}/Panoptes.zip"
     appcast 'https://github.com/jamesdobson/panoptes/releases.atom'
